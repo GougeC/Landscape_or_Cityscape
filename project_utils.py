bca117204272f10c6f0f07d3c5dd541f16bccdf4
Ubuntu <ubuntu@ip-172-31-7-202.us-west-2.compute.internal>
2018-03-09 23:41:24 +0000

    worked on project utils

--- project_utils.py
+++ project_utils.py
@@ -17,25 +17,30 @@ from keras.applications import ResNet50, resnet50
 from keras.applications import InceptionV3, inception_v3
 
 
-def prep_for_vgg(path):
+def prep_for_model(path, model):
     img = image.load_img(path, target_size = (224, 224) )
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis = 0)
-    x = vgg16.preprocess_input(x, mode='tf')
+    if model == 'vgg16':
+        x = vgg16.preprocess_input(x, mode='tf')
+    elif model == 'inception_v3':
+        x = inception_v3.preprocess_input(x, mode='tf')
+    else: 
+        x = resnet50.preprocess_input(x, mode='tf')
     return x
 
-def get_test():
+def get_test(suffix,model):
     '''Creates a test set from a folder I set aside to test results after modeling'''
     
-    citypaths = os.listdir(path='./images/test/city')
-    earthpaths = os.listdir(path='./images/test/earth')
+    citypaths = os.listdir(path='./images/test/city'+suffix)
+    earthpaths = os.listdir(path='./images/test/earth'+suffix)
     X_test, y_test = [],[]
     for path in citypaths:
-        x = prep_for_vgg('./images/test/city/'+path)
+        x = prep_for_model('./images/test/'+path,model)
         X_test.append(x.reshape(224,224,3))
         y_test.append(0)
     for path in earthpaths:
-        x = prep_for_vgg('./images/test/earth/'+path)
+        x = prep_for_model('./images/test/earth/'+path,model)
         X_test.append(x.reshape(224,224,3))
         y_test.append(1)
     X_test, y_test = np.array(X_test), keras.utils.to_categorical(np.array(y_test))
